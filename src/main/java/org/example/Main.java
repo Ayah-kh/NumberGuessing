e056c3e4d54d5ab68a7d8c35b0c2e5c504893c5c
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-03-16 10:25:14 +0200

    First Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int x = 1;
-        System.out.println(x);
+        int rightNumber = 5;
+        int guess=0;
+        Scanner scanner=new Scanner(System.in);
+
+        while (true){
+            System.out.println("enter you guess: ");
+            guess= scanner.nextInt();
+            if (guess==rightNumber){
+                System.out.println("your guess are right");
+                break;
+            }
+        }
     }
 }
